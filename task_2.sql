faacbfd49d67d48701664b2bb33fa20beb83a922
Natnael Mesele <nmesele5@gmail.com>
2024-10-20 15:21:52 +0300

    fix the foreign key format in task2 sql script

--- task_2.sql
+++ task_2.sql
@@ -26,8 +26,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id)
-        REFERENCES Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details (
@@ -35,8 +34,6 @@ CREATE TABLE Order_Details (
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id)
-        REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id)
-        REFERENCES Books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
