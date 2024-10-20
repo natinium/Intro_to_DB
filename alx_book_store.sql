fa203946c457e222acf4d354a1e0697e196b63e4
Natnael Mesele <nmesele5@gmail.com>
2024-10-20 14:33:54 +0300

    fix the customers table address datatype to TEXT

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -20,7 +20,7 @@ CREATE TABLE Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
-    address VARCHAR(215)
+    address TEXT
 );
 
 CREATE TABLE Orders (
